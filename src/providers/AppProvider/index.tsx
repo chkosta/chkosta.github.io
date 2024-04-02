f34e8ee7d09445bcd69bd24c02fb559ede350d51
Christos Costa <christos_costa@hotmail.com>
2024-04-02 23:59:26 +0300

    Fix Routing Bug

--- src/providers/AppProvider/index.tsx
+++ src/providers/AppProvider/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ThemeProvider from "../ThemeProvider";
 import Header from "../../components/layouts/Header";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 
 export default function AppeProvider() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <ThemeProvider>
         <CssBaseline />
         <Header />
